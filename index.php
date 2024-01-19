1906a9fd4bdaa660b9ba302798209aa877297fe4
dinavio <dinaviolina99@gmailcom>
2024-01-19 09:39:13 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+include('layouts/tampilan.php')
+?>
+
---
+++ tim.php
@@ -0,0 +1,67 @@
+<div class="container">
+        <div class="section-title">
+          <h2>Team</h2>
+          <h3>Hasil kerjasama<span>Team</span></h3>
+          <p>Mahasiswa Teknik Informatika Fakultas Teknik Universitas Trunojoyo Madura angkatan 2021 dari Bojonegoro untuk Bojonegoro. </p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>210411100004</h4>
+                <span>Dina Violina</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>210411100183</h4>
+                <span>Oki Bagus</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>210411100195</h4>
+                <span>Dohan Rizqi Hadityo</span>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
